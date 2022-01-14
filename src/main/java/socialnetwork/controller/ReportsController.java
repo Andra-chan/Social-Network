6605327f06403f49605300644c6373a2c85096b7
Alexandru Mercan <alex.mercan2014@yahoo.com>
2022-01-14 23:10:58 +0200

    add date validation

--- src/main/java/socialnetwork/controller/ReportsController.java
+++ src/main/java/socialnetwork/controller/ReportsController.java
@@ -3,11 +3,12 @@ package socialnetwork.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.util.Duration;
 import socialnetwork.App;
 import socialnetwork.Util.controller.FriendListCell;
@@ -17,7 +18,12 @@ import socialnetwork.domain.Message;
 import socialnetwork.service.Service;
 import socialnetwork.service.paging.PageableImplementation;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
+
+import static socialnetwork.Util.Constants.eventDateTime;
 
 public class ReportsController {
     Service service;
@@ -114,6 +120,9 @@ public class ReportsController {
     @FXML
     Button messagesPrevPage;
 
+    @FXML
+    Label warningLabel;
+
     private void setR1Visibility(boolean state) {
         noFriendsImage.setVisible(state);
         noFriendsLabel.setVisible(state);
@@ -154,6 +163,30 @@ public class ReportsController {
         this.userId = userId;
         friendsList.setItems(modelFriendships);
         friendsList.setCellFactory(x -> new FriendListCell());
+        messagesList.setItems(modelMessages);
+        messagesList.setCellFactory(param -> new ListCell<>() {
+            @Override
+            public void updateItem(Message message, boolean empty) {
+                if (empty) {
+                    setGraphic(null);
+                    setText(null);
+                    setStyle("-fx-background-color: #243142");
+                } else {
+                    VBox vbox = new VBox();
+                    vbox.getChildren().clear();
+                    vbox.setPadding(new Insets(15));
+                    Text messageText = new Text(message.getMessageBody());
+                    messageText.getStyleClass().add("fancyText");
+                    TextFlow messageFlow = new TextFlow(messageText);
+                    Text sender = new Text("from: " + message.getFrom().getFirstName() + " "
+                            + message.getFrom().getLastName() + " @ " + message.getDate().format(eventDateTime));
+                    sender.getStyleClass().add("fancyText");
+                    TextFlow senderFlow = new TextFlow(sender);
+                    vbox.getChildren().addAll(senderFlow, messageFlow);
+                    setGraphic(vbox);
+                }
+            }
+        });
         notificationService = new NotificationService(service, userId, notificationsButton,
                 notificationButtonImage, String.valueOf(App.class.getResource("images/notificationsImage.png")),
                 String.valueOf(App.class.getResource("images/activeNotifications.png")));
@@ -162,11 +195,41 @@ public class ReportsController {
     }
 
     public List<Friend> getFriends() {
-        return service.getFriends(userId);
+        return service.getFriendsFilteredPaged(userId, "", "",
+                new PageableImplementation(currentFriendsPage, friendsPageSize));
     }
 
     private void updateFriendsList() {
-        modelFriendships.setAll(service.getFriendsFilteredPaged(userId, "", "", new PageableImplementation(currentFriendsPage, friendsPageSize)));
+        modelFriendships.setAll(getFriends());
+
+        noFriendsLabel.setVisible(modelFriendships.size() == 0);
+        noFriendsImage.setVisible(modelFriendships.size() == 0);
+        friendsList.setVisible(modelFriendships.size() != 0);
+    }
+
+    public List<Message> getReceivedMessages() {
+        return service.getReceivedMessagesFromTimePeriodPaged(userId,
+                LocalDateTime.of(startDate.getValue(), LocalTime.of(0, 0)),
+                LocalDateTime.of(endDate.getValue(), LocalTime.of(23, 59, 59)),
+                new PageableImplementation(currentMessagesPage, messagesPageSize));
+    }
+
+    public void updateReceivedMessaged() {
+        modelMessages.setAll(getReceivedMessages());
+        noMessagesImage.setVisible(modelMessages.size() == 0);
+        noMessagesLabel.setVisible(modelMessages.size() == 0);
+        messagesList.setVisible(modelMessages.size() != 0);
+    }
+
+    public List<Friend> getFriendsFiltered() {
+        return service.getNewFriendsFromTimePeriodPaged(userId,
+                LocalDateTime.of(startDate.getValue(), LocalTime.of(0, 0)),
+                LocalDateTime.of(endDate.getValue(), LocalTime.of(23, 59, 59)),
+                new PageableImplementation(currentFriendsPage, friendsPageSize));
+    }
+
+    private void updateFriendsFiltered() {
+        modelFriendships.setAll(getFriendsFiltered());
         noFriendsLabel.setVisible(modelFriendships.size() == 0);
         noFriendsImage.setVisible(modelFriendships.size() == 0);
         friendsList.setVisible(modelFriendships.size() != 0);
@@ -189,6 +252,17 @@ public class ReportsController {
         currentMessagesPage = 0;
         currentFriendsPage = 0;
         setR1Visibility(false);
+        LocalDate now = LocalDate.now();
+        if (startDate == null || endDate == null
+                || startDate.getValue().isAfter(now)
+                || endDate.getValue().isAfter(now)
+                || startDate.getValue().isAfter(endDate.getValue())){
+            warningLabel.setVisible(true);
+            warningLabel.setText("Invalid date!");
+        }
+        updateFriendsFiltered();
+        updateReceivedMessaged();
+        warningLabel.setVisible(false);
     }
 
     public void onPreviewR2ButtonClick() {
@@ -197,6 +271,32 @@ public class ReportsController {
         currentFriendsPage = 0;
         stateR2Visibility(false);
         updateFriendsList();
+    }
+
+    public void onFriendsPreviousPageButtonClick() {
+        if (currentFriendsPage == 0) {
+            return;
+        }
+        currentFriendsPage--;
+        modelFriendships.setAll(getFriends());
+    }
+
+    public void onFriendsNextPageButtonClick() {
+        currentFriendsPage++;
+        var friends = getFriends();
+        if (friends.isEmpty()) {
+            currentFriendsPage--;
+            return;
+        }
+        modelFriendships.setAll(friends);
+    }
+
+    public void onMessagesNextPageButtonClick() {
+
+    }
+
+    public void onMessagesPreviousPageButtonClick() {
+
     }
 
     public void onExportR1ButtonClick() {
